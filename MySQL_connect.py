66c7fc93a7963d9a76389f2708e6b92d6cf37982
asusgup <48474389+asusgup@users.noreply.github.com>
2024-10-27 15:53:18 +0530

    Update MySQL_connect.py

--- MySQL_connect.py
+++ MySQL_connect.py
@@ -1,5 +1,6 @@
 from jproperties import Properties
 import mysql.connector
+import logging
 
 
 configs = Properties()
@@ -10,16 +11,17 @@ with open('mysql-config.properties', 'rb') as config_file:
 def build_Mysql_Connection():
 	global configs
 	try:
+		logging.info("Initialising mysql connector")
 		connection = mysql.connector.connect(host=configs.get("mysql_host").data,
 		                                     database=configs.get("mysql_report_database").data,
 		                                     user=configs.get("mysql_user").data,
 		                                     password=configs.get("mysql_password").data)
 		if connection.is_connected():
-			print(connection)
+			logging.info(connection)
 			db_Info = connection.get_server_info()
-			print("Connected to MySQL Server version ", db_Info)
+			logging.info("Connected to MySQL Server version ", db_Info)
 			return connection, connection.cursor()
 	except Exception as err:
-	   print("Connection to MySQL DB failed. ERROR - "+ str(err))	
+	   logging.error("Connection to MySQL DB failed. ERROR - "+ str(err))	
 	finally:
 		pass   	
