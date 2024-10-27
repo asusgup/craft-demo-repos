5578806fe45dbafa86ddd0d469d49473ca2b4b23
asusgup <48474389+asusgup@users.noreply.github.com>
2024-10-27 16:04:39 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,64 @@
+{
+  "type": "record",
+  "name": "Record",
+  "fields": [
+    {
+      "name": "id",
+      "type": "string"
+    },
+    {
+      "name": "type",
+      "type": "string"
+    },
+    {
+      "name": "name",
+      "type": "string"
+    },
+    {
+      "name": "ppu",
+      "type": "double"
+    },
+    {
+      "name": "batters.batter",
+      "type": {
+        "type": "array",
+        "items": {
+          "type": "record",
+          "namespace": "Record.batters",
+          "name": "batter",
+          "fields": [
+            {
+              "name": "id",
+              "type": "string"
+            },
+            {
+              "name": "type",
+              "type": "string"
+            }
+          ]
+        }
+      }
+    },
+    {
+      "name": "topping",
+      "type": {
+        "type": "array",
+        "items": {
+          "type": "record",
+          "namespace": "Record",
+          "name": "topping",
+          "fields": [
+            {
+              "name": "id",
+              "type": "string"
+            },
+            {
+              "name": "type",
+              "type": "string"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}
